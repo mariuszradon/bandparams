af75673b4a61bebd03ba4ee49bc81db6a5160443
Mariusz Radon <mariusz.radon@uj.edu.pl>
2022-09-29 18:17:41 +0200

    Improve bandparams: read_spc, arguments, formatting
    Function read_spc to properly read spectrum as dataframe.
    Improved command line arguments and formatting output.

--- bandparams.py
+++ bandparams.py
@@ -57,38 +57,56 @@ def bandparams(df, colname=None):
         'max_val': max_val,
     }
 
+def read_spc(filepath_or_buffer):
+    """Read spectrum from file and return dataframe
+    
+    Parameters:
+    -----------
+    filepath_or_buffer : str, path object or file-like object
+             file to read
+
+    Returns:
+    --------
+    DataFrame
+          spectrum as dataframe with column names 'x' and 'y'
+    """
+    return pd.read_table(
+        filepath_or_buffer,
+        sep='\s+', index_col=False, names=['x', 'y']).set_index('x')
+
+
 def main():
     ap = argparse.ArgumentParser('bandparams')
     ap.add_argument('datafile', metavar='DATAFILE', type=argparse.FileType('rt'),
                         help="ASCII with a band data in table format (x y)")
-    ap.add_argument('--barycenter', '-com', '-b', action='store_const',
+    ap.add_argument('--barycenter', '-com', '-b', '-bc', action='store_const',
                         dest='only_print', const='barycenter',
                         help='only print the barycenter')
-    ap.add_argument('--max_pos', '-x0', action='store_const',
+    ap.add_argument('--max-pos', '--max_pos', '-x0', action='store_const',
                         dest='only_print', const='max_pos',
                         help='only print position of the maximum')
-    ap.add_argument('--max_val', '-y0', action='store_const',
+    ap.add_argument('--max-val', '--max_val', '-y0', action='store_const',
                         dest='only_print', const='max_val',
                         help='only print maximum value')
-    ap.add_argument('--fhwm', '-w', action='store_const',
+    ap.add_argument('--fwhm', '-w', action='store_const',
                         dest='only_print', const='fwhm',
                         help='only print FWHM')
-    ap.add_argument('--x-digits', metavar='M', type=int, default=2,
-                        help='display abscissa variable (x) with M digits')
-    ap.add_argument('--y-digits', metavar='N', type=int, default=3,
-                        help='display ordinate variable (y) with N digits')
+    ap.add_argument('--format-x', metavar='FORMAT', type=str, default='.4f',
+                        help='format for printing the abscissa variable (x)')
+    ap.add_argument('--format-y', metavar='FORMAT', type=str, default='.4e',
+                        help='format for printing the ordinate variable (y)')
     
     args = ap.parse_args()
-    df = pd.read_table(args.datafile, sep='\s+', index_col=False, names=['x', 'y']).set_index('x')
+    df = read_spc(args.datafile)
     bp = bandparams(df)
     for k in bp:
         if k == 'max_val':
-            digits = args.y_digits
+            fmt = args.format_y
         else:
-            digits = args.x_digits
+            fmt = args.format_x
         if args.only_print is None:
-            print(f"{k+':':12s} {round(bp[k],digits)}")
+            print(f"{k+':':12s} {format(bp[k],fmt)}")
         elif k == args.only_print:
-            print(f"{round(bp[k],digits)}")
+            print(f"{format(bp[k],fmt)}")
 
         
